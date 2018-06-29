8604937ab0c7df58632d6fe3c5ff5c637920ee57
SilvaNetoo <silva.neto.sistemas@gmail.com>
2018-06-29 02:02:49 -0300

    Iniciando lógica de logins

--- coffeeSnugPlace/src/app/app.module.ts
+++ coffeeSnugPlace/src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.router';
-import { HomeComponent } from './home/home.component';
-
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginGarcomComponent } from './components/login-garcom/login-garcom.component';
+import { LoginCozinheiroComponent } from './components/login-cozinheiro/login-cozinheiro.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    LoginGarcomComponent,
+    LoginCozinheiroComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(APP_ROUTES),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- coffeeSnugPlace/src/app/app.router.ts
+++ coffeeSnugPlace/src/app/app.router.ts
@@ -1,9 +1,15 @@
+import { LoginGarcomComponent } from './components/login-garcom/login-garcom.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginCozinheiroComponent } from './components/login-cozinheiro/login-cozinheiro.component';
 
 export const APP_ROUTES : Routes =[
     //Rota principal
     { path:'home', component: HomeComponent},
+
+    //Rota de login
+    { path: 'login-garcom', component: LoginGarcomComponent},
+    { path: 'login-cozinheiro', component: LoginCozinheiroComponent },
 
     { path: '**', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
