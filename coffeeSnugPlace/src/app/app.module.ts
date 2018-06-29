529e407c6f8d0a9a8091f6f33e6a6226d3528afb
SilvaNetoo <silva.neto.sistemas@gmail.com>
2018-06-29 00:48:39 -0300

    Corrigindo chamada de component inexistente

--- coffeeSnugPlace/src/app/app.module.ts
+++ coffeeSnugPlace/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.router';
 import { HomeComponent } from './home/home.component';
-import { LoginGarcomComponent } from './login-garcom/login-garcom.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginGarcomComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
