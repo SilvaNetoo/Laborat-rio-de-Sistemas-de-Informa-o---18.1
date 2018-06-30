a1639e85ecce6313d4e8cb8e27c0e4f5d0068333
ArthurOcg <arthurlimaoliveira@ccc.ufcg.edu.br>
2018-06-30 18:28:09 -0300

    Models

---
+++ coffeeSnugPlace/src/main/java/com/br/coffeeSnugPlace/models/Garcom.java
@@ -0,0 +1,9 @@
+package com.br.coffeeSnugPlace.models;
+
+public class Garcom extends Pessoa{
+
+	public Garcom(){
+		super();
+	}
+	
+}
---
+++ coffeeSnugPlace/src/main/java/com/br/coffeeSnugPlace/models/Pessoa.java
@@ -0,0 +1,70 @@
+package com.br.coffeeSnugPlace.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private int idade;
+	private String email;
+	private String senha;
+	
+	public Pessoa(){
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String getSenha() {
+		return senha;
+	}
+
+	private void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean validaLogin(String email, String senha){
+		if(email == this.getEmail() && senha == this.getSenha()){
+			return true;
+		}return false;
+	}
+	
+	
+}
