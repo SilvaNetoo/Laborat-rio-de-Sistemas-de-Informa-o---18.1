569639af76070907da0a1ce9843a465850068a8d
saviohcosta <saviohcosta@gmail.com>
2018-07-02 22:52:12 -0300

    Repo

---
+++ coffeeSnugPlace/src/main/java/com/br/coffeeSnugPlace/repository/PessoaRepository.java
@@ -0,0 +1,10 @@
+package com.br.coffeeSnugPlace.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.br.coffeeSnugPlace.models.Pessoa;
+
+public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
+
+	
+}
